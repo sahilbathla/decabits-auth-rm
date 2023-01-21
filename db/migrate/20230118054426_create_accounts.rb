fc8e381a62e43ff0e9078c06fe4c0e5f21bdfec0
Suparth Jain <suparthjain22@gmail.com>
2023-01-21 11:30:41 +0530

    rails app removed from repository

--- db/migrate/20230118054426_create_accounts.rb
+++
@@ -1,9 +0,0 @@
-class CreateAccounts < ActiveRecord::Migration[7.0]
-  def change
-    create_table :accounts do |t|
-      t.text :user_id
-      t.string :encrypted_password
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230118123815_change_password_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangePasswordColumnName < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :accounts, :encrypted_password, :password_digest
-  end
-end
